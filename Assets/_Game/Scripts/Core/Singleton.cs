da1b603c7e36ff04cb9fb22b41a5f3b30c9150c5
TrungNC <crazytrunk19@gmail.com>
2024-03-22 10:43:10 +0700

    Init Projects

---
+++ Assets/_Game/Scripts/Core/Singleton.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static bool m_ShuttingDown = false;
+
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (m_ShuttingDown)
+            {
+                return null;
+            }
+            if (instance == null)
+            {
+                instance = FindObjectOfType<T>();
+                if (instance == null)
+                {
+                    instance = new GameObject(typeof(T).Name + " (Singleton)").AddComponent<T>();
+                }
+            }
+            return instance;
+        }
+    }
+    private void OnApplicationQuit()
+    {
+        m_ShuttingDown = true;
+    }
+    private void OnDestroy()
+    {
+        m_ShuttingDown = true;
+    }
+}
